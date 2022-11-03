e48cd0a8559b7fdfa0d8bf2b30a54c79b9f0deed
seoo2001 <seoo2001@naver.com>
2022-11-03 21:23:25 +0900

    22-11-03

--- BOJ/test.py
+++ BOJ/test.py
@@ -1,15 +1,3 @@
-import time
-import sys
-
-for i in range(10):
-  print (i)
-  if i == 5:
-    print ("Flushing buffer")
-    sys.stdout.flush()
-  time.sleep(1)
-
-for i in range(10):
-  print (i)
-  if i == 5:
-    print ("Flushing buffer")
-    sys.stdout.flush()
+visited = [[[0]*10]*10]*10
+for i in visited:
+  print(i)
