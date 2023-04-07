14bddbec3a81242c4144957ce8a69f1fcf365368
seoo2001 <seoo2001@naver.com>
2023-04-07 22:41:49 +0900

    최솟값과 최댓값(segment tree 기초)골드 1

--- 2357.py
+++ 2357.py
@@ -16,14 +16,13 @@ def init(start, end, node):
     tree[node][0] = min(tree[node*2][0], tree[node*2+1][0])
     tree[node][1] = max(tree[node*2][1], tree[node*2+1][1])
 init(0,N-1,1)
-print(tree)
 def cal(start, end, node, left, right,f):
     if end < left or right < start: return 0 if f else 1e9
     elif left<=start and end<=right: return tree[node][f]
     mid = (start+end)//2
     if not f: return min(cal(start,mid,node*2,left,right,f),cal(mid+1,end,node*2+1,left,right,f))
     return max(cal(start,mid,node*2,left,right,f),cal(mid+1,end,node*2+1,left,right,f))
-    
+
     
 for _ in range(M):
     a, b = map(int, rl().split())
