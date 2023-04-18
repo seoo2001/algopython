8fbf565b022b97fcc4b65b1e4fff1a03334726d5
seoo2001 <seoo2001@naver.com>
2023-04-18 23:25:25 +0900

    네트워크유량, 정점분할
    정점의 방문 횟수를 체크할 때는 정점분할 사용. in: 2x, out: 2x+1

--- 2316.py
+++ 2316.py
@@ -1,26 +1,29 @@
 import sys
 rl = sys.stdin.readline
 N, P = map(int, rl().split())
-c = [[0 for _ in range(N+1)] for _ in range(N+1)]
+MN = N*2+2
+c = [[0 for _ in range(MN)] for _ in range(MN)]
+for i in range(2,MN,2): c[i][i+1] = 1
+c[2][3], c[4][5] = 1e9, 1e9
 for _ in range(P):
     a, b = map(int, rl().split())
-    c[a][b], c[b][a] = 1, 1
+    c[a*2+1][b*2], c[b*2+1][a*2] = 1, 1
 
-f = [[0 for _ in range(N+1)] for _ in range(N+1)]
+f = [[0 for _ in range(MN)] for _ in range(MN)]
 ans = 0
 while True:
-    q = [1]
-    p = [-1 for _ in range(N+1)]
-    p[1] = 1
+    q = [2]
+    p = [-1 for _ in range(MN)]
+    p[2] = 2
     while q:
         now = q.pop()
-        for i in range(1,N+1):
+        for i in range(1,MN):
             if c[now][i] - f[now][i]>0 and p[i] == -1:
                 q.append(i)
                 p[i] = now
-    if p[2] == -1: break
-    x = 2
-    while x != 1:
+    if p[5] == -1: break
+    x = 5
+    while x != 2:
         f[x][p[x]] -= 1
         f[p[x]][x] += 1
         x = p[x]
